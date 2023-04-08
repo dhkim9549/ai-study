3fa092febe22a9f025a28bbdcdc39c4ca81401ff
root <dhkim95@hotmail.com>
2023-04-08 22:49:57 +0900

    fix some

--- test/test-nn.py
+++ test/test-nn.py
@@ -7,7 +7,7 @@ import numpy as np
 
 # training data 
 rng = np.random.default_rng()
-X = rng.integers(2, size=(10, 2))
+X = rng.integers(2, size=(20, 2))
 X = X.astype(float)
 print(f'X = {X}')
 
@@ -26,15 +26,16 @@ def diffRelu(x):
 
 A = rng.random((2, 5)) * 2 - 1
 B = rng.random((5, 1)) * 2 - 1
-alpha = 0.1
+alpha = 0.01
 
 
 # train NN
-for i in range(100):
+for i in range(10000):
 
     error = 0
 
-    if(i % 10 == 1):
+    if(i % 1000 == 1):
+        print()
         print(f'i = {i}')
 
     for n in range(len(X)):
@@ -50,12 +51,12 @@ for i in range(100):
 
         # backpropagation
         B_delta = output - y
-        B -= alpha * B_delta * h.T
+        B -= alpha * h.T @ B_delta
 
-        A_delta = B_delta * diffRelu(h) * B.T
+        A_delta = B_delta * diffRelu(x @ A) * B.T
         A -= alpha * x.T @ A_delta
 
-    if(i % 10 == 1):
+    if(i % 1000 == 1):
         print(f'x = {x}')
         print(f'h = {h}')
         print(f'y = {y}')
